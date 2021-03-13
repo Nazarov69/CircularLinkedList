91ff76e089cf35d08efb858636c5b40363f7bfaa
Nazarov69 <nniks1837@mail.ru>
2021-03-13 10:33:09 +0300

    CLL

---
+++ main/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "TwoPointers.h"
+#include <time.h>
+#include <stdlib.h>
+
+int main() {
+    int length; bool cycle;
+    cout << "list length : "; cin >> length;
+    int* array = new int[length];
+    for (int i = 0; i < length; i++) array[i] = i + 1;
+    cout << "cycle? "; cin >> cycle;
+    CLL _cll(array, length, cycle);
+    cout << "CLL_racers : ";
+    if (_cll.CLL_racers(_cll)) cout << "CLL" << endl;
+    else cout << "NO CLL" << endl;
+    _cll.ViewCLL(_cll);
+    cout << "CLL_reversible : ";
+    if (_cll.CLL_revers(_cll)) cout << "CLL" << endl;
+    else cout << "NO CLL" << endl;
+    _cll.ViewCLL(_cll);
+   
+
+}
